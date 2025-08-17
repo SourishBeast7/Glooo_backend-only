8d6ed96a31b5017bf84953c3db6677bccdcfe300
SourishBeast7 <aayushpal.singh12@gmail.com>
2025-08-17 19:06:00 +0530

    fixed the branch issue

--- http-server/server.go
+++ http-server/server.go
@@ -212,6 +212,9 @@ func (s *Server) handleApiRoutes(router *mux.Router) {
 	router.HandleFunc("/search-user/{email}", m.AuthMiddleWare(makeHttpHandler(func(w http.ResponseWriter, r *http.Request) error {
 		email := mux.Vars(r)["email"]
 		cookie, err := r.Cookie("id")
+		if err != nil {
+			return err
+		}
 		id := StringToUint(cookie.Value)
 		users, err := s.store.FindUsersUsingSubstring(id, email)
 		if err != nil {
